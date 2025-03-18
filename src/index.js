601f8fac048f8eb6f827cf15a07871650e5ef28d
savi-creates <savannahandresson@gmail.com>
2025-03-18 17:11:56 +0000

    injected weather data

--- src/index.js
+++ src/index.js
@@ -7,6 +7,34 @@ function refreshWeather(response) {
   let windSpeedElement = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let weatherIconElement = document.querySelector(".weather-icon img");
+
+  let iconMapping = {
+    "clear-sky-day": "icons/sun.gif",
+    "clear-sky-night": "icons/sun.gif",
+    "few-clouds-day": "icons/sun-clouds.gif",
+    "few-clouds-night": "icons/sun-clouds.gif",
+    "scattered-clouds-day": "icons/cloudy.gif",
+    "scattered-clouds-night": "icons/cloudy.gif",
+    "broken-clouds-day": "icons/cloudy.gif",
+    "broken-clouds-night": "icons/cloudy.gif",
+    "shower-rain-day": "icons/rain.gif",
+    "shower-rain-night": "icons/rain.gif",
+    "rain-day": "icons/sun-rain.gif",
+    "rain-night": "icons/rain.gif",
+    "thunderstorm-day": "icons/storm.gif",
+    "thunderstorm-night": "icons/storm.gif",
+    "snow-day": "icons/snow.gif",
+    "snow-night": "icons/snow.gif",
+    "mist-day": "icons/cloudy.gif",
+    "mist-night": "icons/cloudy.gif",
+  };
+
+  let weatherIcon = response.data.condition.icon;
+  let iconSrc = iconMapping[weatherIcon] || "icons/cloudy.gif";
+
+  weatherIconElement.src = iconSrc;
+  weatherIconElement.alt = response.data.condition.description;
 
   cityElement.innerHTML = response.data.city;
   timeElement.innerHTML = formatDate(date);
@@ -14,6 +42,57 @@ function refreshWeather(response) {
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function refreshForecast(response) {
+  let forecastElement = document.querySelector(".weekly-forecast");
+  let forecastData = response.data.daily;
+
+  let iconMapping = {
+    "clear-sky-day": "icons/sun.gif",
+    "clear-sky-night": "icons/sun.gif",
+    "few-clouds-day": "icons/sun-clouds.gif",
+    "few-clouds-night": "icons/sun-clouds.gif",
+    "scattered-clouds-day": "icons/cloudy.gif",
+    "scattered-clouds-night": "icons/cloudy.gif",
+    "broken-clouds-day": "icons/cloudy.gif",
+    "broken-clouds-night": "icons/cloudy.gif",
+    "shower-rain-day": "icons/rain.gif",
+    "shower-rain-night": "icons/rain.gif",
+    "rain-day": "icons/sun-rain.gif",
+    "rain-night": "icons/rain.gif",
+    "thunderstorm-day": "icons/storm.gif",
+    "thunderstorm-night": "icons/storm.gif",
+    "snow-day": "icons/snow.gif",
+    "snow-night": "icons/snow.gif",
+    "mist-day": "icons/cloudy.gif",
+    "mist-night": "icons/cloudy.gif",
+  };
+
+  let forecastHTML = "";
+
+  forecastData.forEach((day, index) => {
+    if (index < 7) {
+      let date = new Date(day.time * 1000);
+      let daysOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      let weekday = daysOfWeek[date.getDay()];
+
+      let maxTemp = Math.round(day.temperature.maximum);
+      let minTemp = Math.round(day.temperature.minimum);
+      let weatherIcon = day.condition.icon;
+      let iconSrc = iconMapping[weatherIcon] || "icons/cloudy.gif";
+
+      forecastHTML += `
+        <div class="day">
+          <p>${weekday}</p>
+          <img src="${iconSrc}" alt="${day.condition.description}" />
+          <p><strong>${maxTemp}º</strong> <span>${minTemp}º</span></p>
+        </div>
+      `;
+    }
+  });
+
+  forecastElement.innerHTML = forecastHTML;
 }
 
 function formatDate(date) {
@@ -39,15 +118,21 @@ function formatDate(date) {
 
 function searchCity(city) {
   let apiKey = "ta4d13o783b04c3ee4a956ed2febde0f";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(refreshWeather);
+  let currentWeatherUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let forecastUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(currentWeatherUrl).then(refreshWeather).catch(handleError);
+  axios.get(forecastUrl).then(refreshForecast).catch(handleError);
 }
 
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-
   searchCity(searchInput.value);
+}
+
+function handleError(error) {
+  alert("City not found! Please enter a valid city name.");
 }
 
 let searchFormElement = document.querySelector("#search-form");
